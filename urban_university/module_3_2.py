48170cb6a21e8903b82a69ccd213b9e2ef62c7ca
MiB <MiB@MIBPC>
2024-11-14 19:56:57 +0300

    3-2

---
+++ urban_university/module_3_2.py
@@ -0,0 +1,23 @@
+# coding: cp1251
+
+def EmailValidate(email):
+    return (("@" in str(email))and((".com" in str(email))or(".ru" in str(email))or(".net" in str(email))))
+
+def SendEmail(message, recipient, *,sender = "university.help@gmail.com"):
+    global defaultSender
+    if (not EmailValidate(recipient))or(not EmailValidate(sender)):
+        print(f"Невозможно отправить письмо с адреса <{sender}> на адрес <{recipient}>")
+        return
+    if str(sender)==str(recipient):
+        print("Нельзя отправить письмо самому себе!")
+        return
+    if not str(sender)== defaultSender:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <{sender}> на адрес <{recipient}>.")
+    else:
+        print(f"Письмо успешно отправлено с адреса <sender> на адрес <{recipient}>.")
+
+defaultSender="university.help@gmail.com"
+SendEmail('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+SendEmail('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+SendEmail('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+SendEmail('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
