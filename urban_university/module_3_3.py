9b584e04cf2154f56b11c4c6471903daceec9315
MiB <MiB@MIBPC>
2024-11-17 00:13:47 +0300

    3-3

---
+++ urban_university/module_3_3.py
@@ -0,0 +1,22 @@
+# coding: cp1251
+def PrintParams(a = 1, b = 'строка', c = True):
+    print(f"a={a}\nb={b}\nc={c}",end="\n\n")
+
+def Main3_3():
+    print("Вызов PrintParams")
+    PrintParams()
+    PrintParams(0)
+    PrintParams(0,'asdf')
+    PrintParams(0,'asdf',False)
+    PrintParams(b=25)
+    PrintParams(c=[1,2,3])
+
+    print("Распаковка")
+    values_list=[0, 'строка0', False]
+    values_dict={'a' : 1, 'b' : 'строка', 'c' : True}
+    PrintParams(*values_list)
+    PrintParams(**values_dict)
+
+    print("Распаковка2")
+    values_list_2 =[0, 'строка1']
+    PrintParams(*values_list_2,42)
--- urban_university/urban_university.py
+++ urban_university/urban_university.py
@@ -1,1 +1,4 @@
+import module_3_3
 
+
+module_3_3.Main3_3()
