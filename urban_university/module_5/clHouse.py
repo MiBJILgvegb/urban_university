d10a7766b497a4ca8b3198636a2e0a77fe63946a
MiB <MiB@MIBPC>
2024-12-06 21:40:47 +0300

    5.1

---
+++ urban_university/module_5/clHouse.py
@@ -0,0 +1,24 @@
+﻿class House:
+    def __init__(self,name,number_of_floors):
+        if(not(type(name) is str))or(len(str(name))<=0):
+            print("Имя объекта должно быть строкой длиннее нуля символов.")
+            return None
+        self.name = name
+        
+        if(not(type(number_of_floors) is int))or(number_of_floors<=0):
+            print("Количество этажей должно быть числом больше нуля.")
+            return None
+        self.number_of_floors = number_of_floors
+
+    def go_to(self,new_floor):
+        if(not(type(new_floor) is int)):
+            print("Количество этажей должно быть числом больше нуля.")
+            return False
+        if(new_floor<1)or(new_floor>self.number_of_floors):
+            print("Такого этажа не существует.")
+            return False
+
+        for i in range(1,new_floor+1):
+            print(i)
+        
+        return True
---
+++ urban_university/module_5_1.py
@@ -0,0 +1,7 @@
+﻿from module_5.clHouse import *
+
+house=House(input("Название дома - "),int(input("Количество этажей - ")))
+
+house.go_to(int(input("Куда поедем? - ")))
+
+print(type(int("a")))
