cddf78347cbe66b0a89c87fbe2b58189e3c3b8d0
MiB <MiB@MIBPC>
2025-01-07 11:06:49 +0300

    9.3

---
+++ urban_university/module_9_3.py
@@ -0,0 +1,8 @@
+﻿first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result=(len(z1)-len(z2) for z1,z2 in zip(first,second) if len(z1)!=len(z2))
+second_result=(len(first[i])==len(second[i]) for i in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
